33a5d6b4263b7d13c63e49f36fd1738ffa7a9a55
Pau Larrea <paularrea12@hotmail.com>
2021-09-22 17:57:03 +0200

    detail page style and responsiveness

--- src/pages/detailPage.jsx
+++ src/pages/detailPage.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ContextData } from "../ContextData";
+import { container, text, info_box } from "../styles/pageDetail.module.scss";
 
 const DetailPage = () => {
   const { gnomId } = useParams();
@@ -9,29 +10,62 @@ const DetailPage = () => {
   const gnom =
     gnomData && gnomData.find((gnom) => JSON.stringify(gnom.id) === gnomId);
 
-  console.log(gnom, "gnom");
-
   return (
-    <div>
-      <img src={gnom.thumbnail} alt="" />
-      <h1>{gnom.name}</h1>
-      <p>age: {gnom.age}</p>
-      <p>hair color:{gnom.hair_color}</p>
-      <p>weight: {gnom.weight}</p>
-      <p>height: {gnom.height}</p>
-      <p>
-        Friends List
-        {gnom.friends.map((friend) => {
-          return <li>{friend}</li>;
-        })}
-      </p>
-      <p>
-        Professions
-        {gnom.professions.map((profesion) => {
-          return <li>{profesion}</li>;
-        })}
-      </p>
-    </div>
+    <>
+      {gnom && (
+        <div className={container}>
+          <section>
+            <img src={gnom.thumbnail} alt="" />
+          </section>
+          <section className={text}>
+            <span>Gnome name</span>
+            <h1>{gnom.name}</h1>
+
+            <div className={info_box}>
+              <span>General information</span>
+              <p>
+                <b>age:</b> {gnom.age} years
+              </p>
+              <p>
+                <b>hair color:</b> {gnom.hair_color}
+              </p>
+              <p>
+                <b>weight:</b> {gnom.weight}
+              </p>
+              <p>
+                <b>height:</b> {gnom.height}
+              </p>
+            </div>
+            <div className={info_box}>
+              <p>
+                <span>Friends</span>
+
+                {gnom.friends.length !== 0 ? (
+                  gnom.friends.map((profesion) => {
+                    return <li>{profesion}</li>;
+                  })
+                ) : (
+                  <p>This gnome has no friends</p>
+                )}
+              </p>
+            </div>
+            <div className={info_box}>
+              <p>
+                <span>Professions</span>
+
+                {gnom.professions.length !== 0 ? (
+                  gnom.professions.map((profesion) => {
+                    return <li>{profesion}</li>;
+                  })
+                ) : (
+                  <p>This is a pretty lazy gnome and has no professions</p>
+                )}
+              </p>
+            </div>
+          </section>
+        </div>
+      )}
+    </>
   );
 };
 
